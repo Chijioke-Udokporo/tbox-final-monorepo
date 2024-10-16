4f1c20be4c308fab126d2ded2d0aa27bfceaf4ba
Chijioke Udokporo <77571551+Chijioke-Udokporo@users.noreply.github.com>
2024-10-16 18:57:26 +0100

    remove unstorage

--- apps/backend/src/modules/users/users.routes.ts
+++ apps/backend/src/modules/users/users.routes.ts
@@ -3,12 +3,10 @@ import { zValidator } from "@hono/zod-validator";
 import { responseHandler } from "../../utils/response";
 import { AppConstant } from "../../utils/constant";
 import { z } from "zod";
-import { createStorage } from "unstorage";
 import { IUsers, UsersSchema } from "./users.model";
 import { UserService } from "./users.service";
 import { dbPrisma } from "../../lib/prisma";
 
-const storage = createStorage(/* opts */);
 const service = new UserService(dbPrisma);
 
 export const UserRoutes = new Hono<{
