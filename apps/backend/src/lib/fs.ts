df743e02f6640e255bcdcf6f48188c700411a4bc
Chijioke Udokporo <77571551+Chijioke-Udokporo@users.noreply.github.com>
2024-10-17 09:27:49 +0100

    changed media library

--- apps/backend/src/lib/fs.ts
+++ apps/backend/src/lib/fs.ts
@@ -2,8 +2,7 @@ import * as fs from "node:fs";
 import * as os from "node:os";
 import { join } from "node:path";
 
-//export const homeDir = join(process.cwd(), ".media_files");
-export const homeDir = join(os.homedir(), "media_files");
+export const homeDir = join(process.cwd(), ".media_files");
 export const videosDir = `${homeDir}/videos`;
 export const imagesDir = `${homeDir}/images`;
 export const castDir = `${homeDir}/casts`;
